275c5eb44a2f01fd95664ba202cb67d13d3363ef
Ezra Hampton <ezrahampton@Ezras-MacBook-Pro.local>
2020-10-30 14:57:30 -0400

    working on an express app

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-import express from 'express';
-const app = express;
+import express from "express";
+const app = express();
+// get , put , post, Delete
+
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('hello from the server')
-    req.send('this is the end.');
+app.get("/", (req, res) => {
+    res.send("hello from the server")
+    // res.json()
 })
+app.post('/add', (req, res => {
+    console.log(req.body.id);
+    res.status(200);
+}))
 
 app.listen(port, () => {
-    console.log(`Hello i'm listening from port http://localhost:${port}`);
+    console.log('Hello i\'m listening from port http://localhost:3000');
 } )
